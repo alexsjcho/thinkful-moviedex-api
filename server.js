6f1bf8ede2908a1eeab1add2e5ecbcb26182071e
Alex Cho <alexsjcho@gmail.com>
2019-03-07 17:25:12 -0800

    added .env for authtoken

--- server.js
+++ server.js
@@ -1,1 +1,56 @@
-console.log('hello')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIES = require("./movies-data-small.json");
+
+//Initialize the main app
+const app = express();
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+
+app.use(function validateBearerToken(req, res, next) {
+  //Used process.env to hide UUID token code
+  const apiToken = process.env.API_TOKEN;
+  //Check whether the auth Token matches the UUID token code
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.get("/movie", function handleGetMovie(req, res) {
+  let response = MOVIES;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      //take the Movie response string and turn word to lowercase for case sensitivity
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+});
+
+const PORT = 9000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
